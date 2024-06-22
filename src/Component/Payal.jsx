b79b4b17c797db87ecf4a2c6d8d0ef4540e9951b
Krutikumbhani <krutikumbhani396@gmail.com>
2024-06-22 16:05:48 +0530

    Payal Update

---
+++ src/Component/Payal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Payal = () => {
+    return (
+        <div>
+            <h1>Payal</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, voluptates modi, quia minus, quisquam illo excepturi soluta accusantium doloremque quos quasi sint dolorem repellendus minima consequatur dolorum! Ipsam modi earum, at velit itaque animi sed consectetur doloribus temporibus cum quam dicta veniam nobis eaque, amet excepturi odit dignissimos ratione ut?
+            </p>
+        </div>
+    )
+}
+
+export default Payal;
